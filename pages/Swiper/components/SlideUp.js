cb3aa29e8cf3dc07dc1d858fac1c5684ea3265a4
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2021-08-23 23:03:22 +0300

    change instagram color scheme

--- pages/Swiper/components/SlideUp.js
+++ pages/Swiper/components/SlideUp.js
@@ -24,7 +24,7 @@ const width = height * (9 / 16);
 const SlideUp = () => {
   const dispatch = useDispatch();
 
-  const [colors, setColors] = useState(["#4c669f", "#3b5998", "#192f6a"]);
+  const [activeColor, setActiveColor] = useState("blue");
   const captureRef = useRef(null);
   const translateY = useRef(new Animated.Value(height));
   const quotes = useSelector((state) => state.quotes.items);
@@ -35,23 +35,16 @@ const SlideUp = () => {
     dispatch(setShareInstagramOpen(false));
   };
 
-  const changeColor = (color) => {
-    switch (color) {
-      case "blue": {
-        setColors(["#4c669f", "#3b5998", "#192f6a"]);
-        break;
-      }
-      case "pink": {
-        setColors(["#cf0100", "#f03762", "#ff0095"]);
-        break;
-      }
-
-      case "green": {
-        setColors(["#013900", "#0d6600", "#468800"]);
-        break;
-      }
-    }
+  const colors = {
+    blue: ["#4c669f", "#3b5998", "#192f6a"],
+    pink: ["#bc4e9c", "#f80759"],
+    green: ["#5A3F37", "#2C7744"],
   };
+
+  const changeColor = (color) => {
+    setActiveColor(color);
+  };
+
   const share = (uri) => {
     let urlString = "data:image/jpeg;base64," + uri;
     let options = {
@@ -109,7 +102,10 @@ const SlideUp = () => {
         style={{ flex: 1 }}
         options={{ format: "jpg", quality: 1, result: "base64" }}
       >
-        <LinearGradient colors={colors} style={styles.linearGradient}>
+        <LinearGradient
+          colors={colors[activeColor]}
+          style={styles.linearGradient}
+        >
           <ScrollView
             style={styles.card}
             contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
@@ -146,13 +142,31 @@ const SlideUp = () => {
       >
         <View style={styles.colors}>
           <TouchableOpacity onPress={() => changeColor("green")}>
-            <View style={[styles.green, styles.color]}></View>
+            <View
+              style={[
+                styles.green,
+                styles.color,
+                activeColor === "green" && styles.activeColor,
+              ]}
+            ></View>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => changeColor("pink")}>
-            <View style={[styles.pink, styles.color]}></View>
+            <View
+              style={[
+                styles.pink,
+                styles.color,
+                activeColor === "pink" && styles.activeColor,
+              ]}
+            ></View>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => changeColor("blue")}>
-            <View style={[styles.blue, styles.color]}></View>
+            <View
+              style={[
+                styles.blue,
+                styles.color,
+                activeColor === "blue" && styles.activeColor,
+              ]}
+            ></View>
           </TouchableOpacity>
         </View>
         <View>
@@ -169,13 +183,15 @@ const SlideUp = () => {
 };
 
 const styles = StyleSheet.create({
+  activeColor: {
+    borderWidth: 2,
+    borderColor: "white",
+  },
   color: {
     width: 36,
     height: 36,
     borderRadius: 50,
     marginRight: 10,
-    borderWidth: 1,
-    borderColor: "white",
   },
   colors: {
     opacity: 0.6,
@@ -183,13 +199,13 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   green: {
-    backgroundColor: "green",
+    backgroundColor: "#2C7744",
   },
   blue: {
     backgroundColor: "blue",
   },
   pink: {
-    backgroundColor: "#cf0100",
+    backgroundColor: "#f80759",
   },
   card: {
     flex: 1,
